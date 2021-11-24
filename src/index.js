1bbf5a959158bf981f6b02e4bf5f1ee99243a173
Gr3g03 <71180086+Gr3g03@users.noreply.github.com>
2021-11-24 16:00:04 +0100

    form finished

--- src/index.js
+++ src/index.js
@@ -27,21 +27,24 @@ function getDog(data){
 
     dogdDescription.append(dogBio,dogDataBio )
 
+    const dogSectionDescription= document.createElement('div')
+    dogSectionDescription.setAttribute("class", "main__dog-section__btn")
+
     const emEl = document.createElement('em')
     emEl.textContent = "Is naughty? "
 
     const btnEl = document.createElement('button')
     btnEl.textContent = "Good dog!"
+    btnEl.addEventListener('click', function(){
+        btnEl.textContent = "Bad dog "
+    })
 
     const secondDogParagraph = document.createElement('p')
-    secondDogParagraph.textContent = data.isGoodDog
 
+    secondDogParagraph.textContent += ("yes ")
     secondDogParagraph.append(emEl, btnEl)
+     
   
-
-
-    const dogSectionDescription= document.createElement('div')
-    dogSectionDescription.setAttribute("class", "main__dog-section__btn")
 
     dogSection.append(dogName, dogImage, dogdDescription ,dogSectionDescription, secondDogParagraph)
     
@@ -50,6 +53,55 @@ function getDog(data){
 }
 
 
-for (const dog of data) {
+for (const dog of data) { 
     getDog(dog)
 }
+
+
+function addDog(){
+
+    const dogSection = document.createElement('section')
+    dogSection.setAttribute('class', 'main__dog-section')
+
+    const addDogH2 = document.createElement('h2')
+    addDogH2.textContent = ('Add a new Dog')
+
+    const formElement = document.createElement('form')
+    formElement.setAttribute('class', 'form')
+
+    const labelElement = document.createElement('label')
+    labelElement.setAttribute('for', 'name')
+    labelElement.textContent = (`Dog's name`)
+    const inputElement =document.createElement('input')
+    inputElement.setAttribute('type', 'text')
+    inputElement.setAttribute('type', 'text')
+    inputElement.setAttribute('name', 'name')
+
+    const labelElement2 = document.createElement('label')
+    labelElement2 .setAttribute('for', 'image')
+    labelElement2.textContent = (`Dog's picture`)
+    const inputElement2 =document.createElement('input')
+    inputElement2.setAttribute('type', 'url')
+    inputElement2.setAttribute('type', 'image')
+    inputElement2.setAttribute('name', 'image')
+
+    const labelElement3 = document.createElement('label')
+    labelElement3 .setAttribute('for', 'bio')
+    labelElement3.textContent = (`Dog's bio`)
+    const inputElement3 =document.createElement('textarea')
+    inputElement3.setAttribute('rows', '5')
+    inputElement3.setAttribute('id', 'bio')
+    inputElement3.setAttribute('name', 'bio')
+
+    formElement.append(labelElement, inputElement, labelElement2,inputElement2, labelElement3 ,inputElement3,  )
+    dogSection.append(addDogH2 ,formElement)
+}
+
+
+// function headerDogMenu(){
+//     const headerEl = document.querySelector(".header")
+//     const listElement = document.querySelector(".dogs-list")
+//     const allListElement = document.querySelector(".dogs-list__button dogs-list__button--add")
+// }
+
+addDog()
